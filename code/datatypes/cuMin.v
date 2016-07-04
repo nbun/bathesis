e99c0e8ff3ad2659c58c25c908014aa49fdf1606
Niels Bunkenburg <stu114713@informatik.uni-kiel.de>
2016-07-04 15:51:05 +0200

    Added tag context

--- code/datatypes/cuMin.v
+++ code/datatypes/cuMin.v
@@ -8,7 +8,7 @@ Definition total_map (A:Type) := id -> A.
 
 Definition partial_map (A:Type) := total_map (option A).
 
-Definition t_empty {A:Type} (v : A) : total_map A :=
+Definition tmap_empty {A:Type} (v : A) : total_map A :=
   (fun _ => v).
   
 
@@ -21,12 +21,16 @@ Definition t_update {A:Type} (m : total_map A)
                     (x : id) (v : A) :=
   fun x' => if beq_id x x' then v else m x'.
 
-Definition empty {A:Type} : partial_map A :=
-  t_empty None.
+Definition emptymap {A:Type} : partial_map A :=
+  tmap_empty None.
 
 Definition update {A:Type} (m : partial_map A)
                   (x : id) (v : A) :=
   t_update m x (Some v).
+  
+Inductive tag : Type :=
+ | tag_star  : tag
+ | tag_empty : tag.
 
 Inductive ty : Type :=
   | TVar  : ty
@@ -35,7 +39,29 @@ Inductive ty : Type :=
   | TList : ty -> ty
   | TPair : ty -> ty -> ty
   | TFun  : ty -> ty -> ty.
-  
+
+
+Inductive context : Type := 
+ | con : (partial_map tag) -> (partial_map ty) -> context.
+
+Definition empty := con emptymap emptymap.
+ 
+Definition typecon (Gamma : context) : (partial_map ty) :=
+match Gamma with
+ | (con _ tyc) => tyc
+end.
+
+Definition tagcon (Gamma : context) : (partial_map tag) :=
+match Gamma with
+ | (con tagc _) => tagc
+end.
+
+Definition type_update (Gamma : context) (x : id) (v : ty) := 
+ (con (tagcon Gamma) (update (typecon Gamma) x v)).
+ 
+Definition tag_update (Gamma : context) (x : id) (v : tag) := 
+ (con (update (tagcon Gamma) x v) (typecon Gamma)).
+
 Inductive tm : Type :=
   | tvar   : id -> tm
   | tapp   : tm -> tm -> tm
@@ -55,13 +81,10 @@ Inductive tm : Type :=
   | tcasep : tm -> tm -> tm
   | tcaseb : tm -> tm -> tm -> tm
   | tcasel : tm -> tm -> tm -> tm.
-  
-
-Definition context := partial_map ty.
 
 Reserved Notation "Gamma '|-' T '\in_data'" (at level 40).
 Inductive data : context -> ty -> Prop :=
-  | D_Var :  forall Gamma A, Gamma |- A \in_data (*?*)
+  | D_Var :  forall Gamma A, Gamma |- A \in_data (* insert tag context here... *)
   | D_Bool : forall Gamma, Gamma |- TBool \in_data
   | D_Nat :  forall Gamma, Gamma |- TNat \in_data
   | D_List : forall Gamma T,
@@ -77,7 +100,7 @@ Reserved Notation "Gamma '|-' t '\in' T" (at level 40).
 
 Inductive has_type : context -> tm -> ty -> Prop :=
   | T_Var :    forall Gamma x T,
-               (update Gamma x T) |- tvar x \in T
+               (type_update Gamma x T) |- tvar x \in T
   | T_True :   forall Gamma, 
                Gamma |- ttrue \in TBool
   | T_False :  forall Gamma,
@@ -95,7 +118,7 @@ Inductive has_type : context -> tm -> ty -> Prop :=
                Gamma |- (tapp e1 e2) \in T2
   | T_Let :    forall Gamma e1 e2 x T1 T2,
                Gamma |- e1 \in T1 ->
-               (update Gamma x T1) |- e2 \in T2 ->
+               (type_update Gamma x T1) |- e2 \in T2 ->
                Gamma |- (tlet (tvar x) e1 e2) \in T2
   (*| T_Fun :    forall Gamma TV1 TV2 T1 T2 *)
   | T_Add :    forall Gamma e1 e2,
@@ -117,11 +140,11 @@ Inductive has_type : context -> tm -> ty -> Prop :=
   | T_CaseL :  forall Gamma e e1 e2 h t T T',
                Gamma |- e \in (TList T') ->
                Gamma |- e1 \in T ->
-               (update (update Gamma h T') t (TList T')) |- e2 \in T ->
+               (type_update (type_update Gamma h T') t (TList T')) |- e2 \in T ->
                Gamma |- (tcasel e e1 e2) \in T
   | T_CaseP :  forall Gamma e e1 l r T T1 T2,
                Gamma |- e \in (TPair T1 T2) ->
-               (update (update Gamma l T1) r T2) |- e1 \in T ->
+               (type_update (type_update Gamma l T1) r T2) |- e1 \in T ->
                Gamma |- (tcasep e e1) \in T
   | T_CaseB :  forall Gamma e e1 e2 T,
                Gamma |- e \in TBool ->
@@ -129,10 +152,10 @@ Inductive has_type : context -> tm -> ty -> Prop :=
                Gamma |- e2 \in T ->
                Gamma |- (tcaseb e e1 e2) \in T
   | T_Fail :   forall Gamma T,
-               Gamma |- tfailure \in T
+               Gamma |- (tfail T) \in T
   | T_Any :    forall Gamma T,
                Gamma |- T \in_data ->
-               Gamma |- tanything \in T
+               Gamma |- (tany T) \in T
   
 where "Gamma '|-' t '\in' T" := (has_type Gamma t T).
 
@@ -141,9 +164,9 @@ where "Gamma '|-' t '\in' T" := (has_type Gamma t T).
 Example t1 : empty |- ttrue \in TBool.
 Proof. apply T_True. Qed.
 
-Definition con := (update empty (Id 5) TNat).
+Definition c := (type_update empty (Id 5) TNat).
 
-Example t2 : con |- (tvar (Id 5)) \in TNat.
+Example t2 : c |- (tvar (Id 5)) \in TNat.
 Proof. apply T_Var. Qed.
 
 Example t3 : empty |- (tlet (tvar (Id 5)) tzero (tadd (tsucc tzero) (tvar (Id 5)))) \in TNat.
@@ -155,22 +178,19 @@ Proof. apply T_Let with (T1 := TNat).
 Qed.
 
 Example t4 : empty |- (tcasel tnil tnil (tcons (tsucc tzero) tnil)) \in (TList TNat).
-Proof. apply T_CaseLN with (T' := TNat).
+Proof. apply T_CaseL with (T' := TNat) (h := Id 5) (t := Id 6).
   apply T_Nil.
   apply T_Nil.
   apply T_Cons.
     apply T_Succ. apply T_Zero.
     apply T_Nil.
-  reflexivity.
 Qed.
 
-(* Doesn't work... *)
-Example t5 : (update empty (Id 5) TBool) |- (tcasel (tcons ttrue tnil) tfalse (tvar (Id 5))) \in TBool.
-Proof. apply T_CaseLC.
-  apply T_Nil.
-  apply T_Nil.
+Example t5 : (type_update empty (Id 5) TBool) |- (tcasel (tcons ttrue tnil) tfalse (tvar (Id 5))) \in TBool.
+Proof. apply T_CaseL with (T' := TBool) (h := Id 6) (t := Id 7).
   apply T_Cons.
-    apply T_Succ. apply T_Zero.
+    apply T_True.
     apply T_Nil.
-  reflexivity.
-Qed.
+  apply T_False.
+  (*apply T_Var. *)
+  Admitted.
